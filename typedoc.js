6ae8e14dba568e47b89603eef3bceaa776316b30
naosim <nao_pillows@yahoo.co.jp>
2020-05-09 10:39:05 +0900

    typedocをミニマムに

--- typedoc.js
+++ typedoc.js
@@ -1,13 +1,14 @@
 module.exports = {
-  out: './docs',
+  out: './docs/typedoc',
   name: 'board2d',
   // readme: 'none',
   target: 'ES6',
   includes: './',
   exclude: './node_modules/**',
-  mode: 'modules',
+  mode: 'file',
   //excludeExternals: true,
   //excludeNotExported: true,
   excludePrivate: true,
-  includeDeclarations: true
+  includeDeclarations: true,
+  theme: 'minimal'
 };
